7373a095b4640bfa68f6674aaf7c52c4c40f3976
a7iiid <naz131681@gmail.com>
2024-06-29 11:41:53 +0300

    add driver model

--- lib/feture/google_map/manegar/cubit/driver_cubit.dart
+++ lib/feture/google_map/manegar/cubit/driver_cubit.dart
@@ -1,8 +1,13 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'driver_state.dart';
 
 class DriverCubit extends Cubit<DriverState> {
   DriverCubit() : super(DriverInitial());
+  static DriverCubit get(context) => BlocProvider.of(context);
+  FirebaseFirestore instans = FirebaseFirestore.instance;
 }
